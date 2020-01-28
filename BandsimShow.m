a6197897d56c24e07debd9f1f1230e13153b8821
Hussain119 <hafiz.husain119@gmail.com>
2020-01-28 10:42:06 +0500

    Add files via upload

---
+++ BandsimShow.m
@@ -0,0 +1,35 @@
+function [] = showBandimShow(im2)
+%SHOWBAND Summary of this function goes here
+This file is a part of the Advanced Image Processing Research Lab (KFUEIT) Toolbox for MATLAB
+ 
+% Press n for next band grey-scale Image
+% Press p for previous band grey-scale Image
+% Press b for to end
+
+
+i=1;
+while 1
+    figure(2)
+   imshow(im2(:,:,i),[]);
+   title(i)
+ w = waitforbuttonpress;
+ ch=get(gcf,'CurrentCharacter');
+ disp('you entered');ch
+ i
+ if ch=='n'
+     i=i+1;
+     imshow(im2(:,:,i),[]);
+ elseif ch=='p'
+     if i>=2
+         i=i-1;
+         imshow(im2(:,:,i),[]);
+         title(i)
+     else
+         disp('Error Index')
+     end
+ elseif ch=='b'
+     break
+ end
+end
+end
+
